9dcc44a9a958b4b99b85c694248edb3cd960301f
cocopeanutz <edobiasa@gmail.com>
2018-10-16 22:49:00 +0800

    This is get Location with one time access only with 'getLastLocation' .....last Location only not periodical updates

--- app/src/main/java/com/fromscratch/getlocation/MainActivity.java
+++ app/src/main/java/com/fromscratch/getlocation/MainActivity.java
@@ -1,14 +1,56 @@
 package com.fromscratch.getlocation;
 
+import android.content.Context;
+
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import android.content.IntentSender;
+import android.location.Location;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private FusedLocationProviderClient mFusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location!=null){
+                    Log.v("a","location is not null");
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    Log.v("latitude", String.valueOf(latitude));
+                    Log.v("longitude", String.valueOf(longitude));
+                    String latNLong = String.valueOf(latitude) + String.valueOf(longitude);
+                    Toast toast = Toast.makeText(getApplicationContext(), latNLong, Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                else{
+                    Log.v("a","Location is null");
+                    Toast toast = Toast.makeText(getApplicationContext(), "Location null lol", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        });
     }
 }
 
